249eb3014cdbe0c73ec373f888d1b97a8c14d33a
Keks <keks@htmlacademy.ru>
2022-04-16 00:10:04 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,8 @@
+import { similarDescriptionPhotos} from './data.js';
+import './utils.js';
+import {getData} from './small-picture.js';
+import './form.js';
+import './upload.js';
+
+//console.log(similarDescriptionPhotos);
+getData(similarDescriptionPhotos);
